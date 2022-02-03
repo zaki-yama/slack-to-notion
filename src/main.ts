50297750840fa117f707c1e6203b1d3a6ecce515
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2022-02-04 04:32:47 +0900

    feat: verify challenge

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-function doPost(e) {}
+function doPost(e) {
+  console.log("hello");
+  console.log(e.postData.contents);
+  const contents = JSON.parse(e.postData.contents);
+  return ContentService.createTextOutput(contents.challenge);
+}
+
+function doGet(e) {
+  console.log(e);
+}
